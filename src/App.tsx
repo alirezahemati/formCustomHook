958a75cfc7ef09442f9efa8fd1c3ce6916f2982b
Alireza Hemati <alirezasaxon@gmail.com>
2023-11-08 09:02:20 +0330

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-slate-900 text-white">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
